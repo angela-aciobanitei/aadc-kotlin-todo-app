3c958c13d3e03ee2391c19f00758b5c293f42bb5
Angela Aciobanitei <aciobanitei.angela@gmail.com>
2020-04-21 09:18:36 +0100

    Initial commit

---
+++ app/src/main/java/com/ang/acb/todolearn/ui/ListFragment.kt
@@ -0,0 +1,33 @@
+package com.ang.acb.todolearn.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+
+import com.ang.acb.todolearn.R
+
+class ListFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ListFragment()
+    }
+
+    private lateinit var viewModel: ListViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.list_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/ang/acb/todolearn/ui/ListViewModel.kt
@@ -0,0 +1,7 @@
+package com.ang.acb.todolearn.ui
+
+import androidx.lifecycle.ViewModel
+
+class ListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
