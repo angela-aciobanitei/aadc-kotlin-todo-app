65b1c8dd792b7e952d9ce55f1db89b88c5cc520d
Angela Aciobanitei <aciobanitei.angela@gmail.com>
2020-04-29 13:13:05 +0100

    Use Espresso Idling Resource in tasks repository

--- app/src/main/java/com/ang/acb/todolearn/data/repo/TasksRepository.kt
+++ app/src/main/java/com/ang/acb/todolearn/data/repo/TasksRepository.kt
@@ -6,6 +6,7 @@ import com.ang.acb.todolearn.data.local.Result
 import com.ang.acb.todolearn.data.local.Task
 import com.ang.acb.todolearn.data.local.TasksDataSource
 import com.ang.acb.todolearn.data.local.TasksLocalDataSource
+import com.ang.acb.todolearn.util.wrapEspressoIdlingResource
 import kotlinx.coroutines.*
 
 
@@ -21,70 +22,116 @@ class TasksRepository (
     // does not complete until all launched children complete.
     // https://kotlinlang.org/docs/reference/coroutines/basics.html#scope-builder
     override suspend fun saveTask(task: Task) {
-        coroutineScope {
-            launch { tasksLocalDataSource.saveTask(task) }
+        wrapEspressoIdlingResource {
+            coroutineScope {
+                launch { tasksLocalDataSource.saveTask(task) }
+            }
         }
     }
 
     override suspend fun saveTasks(tasks: List<Task>) {
-        coroutineScope {
-            launch { tasksLocalDataSource.saveTasks(tasks) }
+        wrapEspressoIdlingResource {
+            coroutineScope {
+                launch { tasksLocalDataSource.saveTasks(tasks) }
+            }
         }
     }
 
     override suspend fun updateTask(task: Task) {
-        coroutineScope {
-            launch { tasksLocalDataSource.updateTask(task) }
+        wrapEspressoIdlingResource {
+            coroutineScope {
+                launch { tasksLocalDataSource.updateTask(task) }
+            }
         }
     }
 
     override suspend fun activateTask(task: Task){
-        coroutineScope {
-            launch { tasksLocalDataSource.activateTask(task) }
+        wrapEspressoIdlingResource {
+            coroutineScope {
+                launch { tasksLocalDataSource.activateTask(task) }
+            }
         }
     }
 
     override suspend fun completeTask(task: Task) {
-        coroutineScope {
-            launch { tasksLocalDataSource.completeTask(task) }
+        wrapEspressoIdlingResource {
+            coroutineScope {
+                launch { tasksLocalDataSource.completeTask(task) }
+            }
         }
     }
 
     override suspend fun deleteTask(task: Task) {
-        coroutineScope {
-            launch { tasksLocalDataSource.deleteTask(task) }
+        wrapEspressoIdlingResource {
+            coroutineScope {
+                launch { tasksLocalDataSource.deleteTask(task) }
+            }
         }
     }
 
     override suspend fun deleteTaskById(taskId: String) {
-        coroutineScope {
-            launch { tasksLocalDataSource.deleteTaskById(taskId) }
+        wrapEspressoIdlingResource {
+            coroutineScope {
+                launch { tasksLocalDataSource.deleteTaskById(taskId) }
+            }
         }
     }
 
     override suspend fun deleteAllTasks() {
-        coroutineScope {
-            launch { tasksLocalDataSource.deleteAllTasks() }
+        wrapEspressoIdlingResource {
+            coroutineScope {
+                launch { tasksLocalDataSource.deleteAllTasks() }
+            }
         }
     }
 
     override suspend fun deleteCompletedTasks() {
-        coroutineScope {
-            launch { tasksLocalDataSource.deleteCompletedTasks() }
+        wrapEspressoIdlingResource {
+            coroutineScope {
+                launch { tasksLocalDataSource.deleteCompletedTasks() }
+            }
         }
     }
 
-    override suspend fun getTask(taskId: String): Result<Task> = tasksLocalDataSource.getTask(taskId)
+    override suspend fun getTask(taskId: String): Result<Task> {
+        wrapEspressoIdlingResource {
+            return tasksLocalDataSource.getTask(taskId)
+        }
+    }
 
-    override suspend fun getTasks(): Result<List<Task>> = tasksLocalDataSource.getTasks()
+    override suspend fun getTasks(): Result<List<Task>> {
+        wrapEspressoIdlingResource {
+            return tasksLocalDataSource.getTasks()
+        }
+    }
 
-    override fun getLiveTask(taskId: String): LiveData<Result<Task>> = tasksLocalDataSource.getLiveTask(taskId)
+    override fun getLiveTask(taskId: String): LiveData<Result<Task>> {
+        wrapEspressoIdlingResource {
+            return tasksLocalDataSource.getLiveTask(taskId)
+        }
+    }
 
-    override fun getLiveTasks(): LiveData<Result<List<Task>>> = tasksLocalDataSource.getLiveTasks()
+    override fun getLiveTasks(): LiveData<Result<List<Task>>> {
+        wrapEspressoIdlingResource {
+            return tasksLocalDataSource.getLiveTasks()
+        }
+    }
 
-    override fun getAllPagedTasks(): LiveData<PagedList<Task>> = tasksLocalDataSource.getAllPagedTasks()
+    override fun getAllPagedTasks(): LiveData<PagedList<Task>> {
+        wrapEspressoIdlingResource {
+            return tasksLocalDataSource.getAllPagedTasks()
+        }
+    }
 
-    override fun getCompletedPagedTasks(): LiveData<PagedList<Task>> = tasksLocalDataSource.getCompletedPagedTasks()
+    override fun getCompletedPagedTasks(): LiveData<PagedList<Task>> {
+        wrapEspressoIdlingResource {
+            return tasksLocalDataSource.getCompletedPagedTasks()
+        }
+    }
 
-    override fun getActivePagedTasks(): LiveData<PagedList<Task>> = tasksLocalDataSource.getActivePagedTasks()
+    override fun getActivePagedTasks(): LiveData<PagedList<Task>> {
+        wrapEspressoIdlingResource {
+            return tasksLocalDataSource.getActivePagedTasks()
+        }
+    }
 }
